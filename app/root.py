80d0755d2943c47ea1a727702376dc0f45e190fc
Konstantin Malanchev <hombit@gmail.com>
2020-03-04 13:53:33 +0300

    Add API v2 info to the index page

--- app/root.py
+++ app/root.py
@@ -1,20 +1,41 @@
 from aiohttp.web import Response, RouteTableDef
 
+from .api_version import get_api_versions
 
 routes = RouteTableDef()
 
 
+_template = '''
+<p>
+    Welcome on <a href="//snad.space">SNAD</a> <a href="https://www.ztf.caltech.edu/page/dr2">ZTF DR2</a>
+    light curves page.
+</p>
+{help}
+<p>
+    See source code <a href="https://github.com/hombit/snad-ztf-db/">on GitHub</a>.
+</p>
+'''
+
+
+_help_links = dict(
+    v2='''
+        <p>
+            API v2 provides access to ZTF DR2, see details on <a href="/api/v2/help">/api/v2/help</a>.
+        </p>
+    ''',
+    v1='''
+        <p>
+            Legacy API v1 provides access to ZTF DR1, see details on <a href="/api/v1/help">/api/v1/help</a>.
+        </p> 
+    ''',
+)
+
+
 @routes.get('/')
 async def index(request) -> Response:
+    api_versions = get_api_versions()
+    links = '\n'.join(text for version, text in _help_links.items() if version in api_versions)
     return Response(
-        text='''
-            <p>
-                Welcome on <a href="//snad.space">SNAD</a> <a href="https://www.ztf.caltech.edu/page/dr1">ZTF dr1</a></a>
-            mirror page.
-            </p>
-            <p>
-                See API details on <a href="/api/v1/help">/api/v1/help</a>
-            </p>
-        ''',
+        text=_template.format(help=links),
         content_type='text/html',
     )
