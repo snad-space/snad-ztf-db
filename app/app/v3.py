c273e87d149af401b8b3d6e3016d4253a791fea3
Konstantin Malanchev <hombit@gmail.com>
2025-03-06 09:34:34 -0500

    Make DR23 default

--- app/app/v3.py
+++ app/app/v3.py
@@ -22,7 +22,7 @@ SUPPORTED_DRS = ('dr2', 'dr3', 'dr4', 'dr8', 'dr13', 'dr17', 'dr23')
 _ALL_AVAILABLE_DRS = get_avail_drs()
 AVAILABLE_DRS = tuple(dr for dr in SUPPORTED_DRS if dr in _ALL_AVAILABLE_DRS) + ('latest',)
 SHORT_META_DRS = ('dr2', 'dr3')
-LATEST_DR = 'dr17'
+LATEST_DR = 'dr23'
 AVAILABLE_DRS_HTML = ', '.join(f"<font face='monospace'>{dr}</font>" for dr in AVAILABLE_DRS)
 
 
